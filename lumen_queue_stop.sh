caf139510e31ac7c470fd1ab748f4cb7742f7247
Hung Truong <hungtruongquoc@gmail.com>
2020-10-14 20:35:37 -0500

    20201014: Added a new service jobs

---
+++ lumen_queue_stop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker stop lumen-queue_curator_1
+docker stop lumen-queue_dispenser_1
+docker stop lumen-queue_webserver_1
+docker stop lumen-queue_db_1
+docker stop lumen-queue_redis_1
